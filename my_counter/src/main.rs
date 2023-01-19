d8dcb8018d28a520d41225c08bedee7ad7d5fa14
Vesonify <rkodiriy@students.wiut.uz>
2023-01-20 02:25:05 +0500

    Decrease Button Implemented

--- my_counter/src/main.rs
+++ my_counter/src/main.rs
@@ -1,7 +1,8 @@
 use yew::prelude::*;
 
 enum Msg {
-    AddOne
+    AddOne,
+    DecOne
 }
 struct Counter {
     count: i64
@@ -20,6 +21,10 @@ impl Component for Counter{
             Msg::AddOne => {
                 self.count += 1;
                 true
+            },
+            Msg::DecOne => {
+                self.count -= 1;
+                true
             }
         }
     }
@@ -29,7 +34,8 @@ impl Component for Counter{
         html!{
             <div class="container">
                 <p>{self.count}</p>
-                <button onclick = {link.callback(|_| Msg::AddOne)}>{"+1"}</button>
+                <button class ="add_one" onclick = {link.callback(|_| Msg::AddOne)}>{"+1"}</button>
+                <button class ="dec_one" onclick = {link.callback(|_| Msg::DecOne)}>{"-1"}</button>
             </div>
         }
     }
